f38026c25ca0eeae7efb1efc4860e0b6483296ff
Grace Kalombo <kalombograce1@gmail.com>
2021-07-23 09:30:54 +0200

    Portfolio setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="logo">
+                GK
+            </div>
+            <div className="contact"> 
+                <i className="fas fa-phone"></i>
+                <span>+27 84 048 28 61</span>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
