e59839c8d51793b6e7509849e651853eea26b063
Grace kalombo <kalombograce1@gmail.com>
2021-08-08 03:46:07 +0200

    Retouch contact pages

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,15 @@
 import './AboutMe.css'
+import { Link } from 'react-router-dom'
 
 const AboutMe = () => {
     return (
         <div className="about__me">
             <h2 className="aboutme__title">About Me</h2>
             <p className="aboutme__description">I'm a Goal-oriented Web Developer that brings strong commitment to collaboration and solutions-oriented problem-solving. I used various web design packages to develop custom-crafted, customer-focused websites and designs. Committed to high standards of user experience, usability and speed over more than 3 years for multiple users. Lends detailed knowledge of SEO to increase visibility.</p>
-            <button className="aboutme__btn">GET IN TOUCH</button>
+            <Link to="/contact">
+                <button className="aboutme__btn">GET IN TOUCH</button>
+            </Link>
+
         </div>
     )
 }
